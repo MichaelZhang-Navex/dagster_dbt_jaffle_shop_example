360c4f3013c91696dd147746ea0e90845ec65aa3
Michael Zhang <michael.zhang@navex.com>
2024-04-19 02:10:21 -0500

    add schedule

--- jaffle_dagster/jaffle_dagster/schedules.py
+++ jaffle_dagster/jaffle_dagster/schedules.py
@@ -1,15 +1,16 @@
 """
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
-from dagster_dbt import build_schedule_from_dbt_selection
+from dagster import ScheduleDefinition
+# from dagster_dbt import build_schedule_from_dbt_selection
 
-from .assets import jaffle_shop_dbt_assets
+from .assets import jaffle_shop_dbt_asset_job
 
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [jaffle_shop_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+     ScheduleDefinition(
+         name='data_model_materialization',
+         job=jaffle_shop_dbt_asset_job,
+         cron_schedule="0 0 * * *",
+        #  dbt_select="",
+     ),
 ]
